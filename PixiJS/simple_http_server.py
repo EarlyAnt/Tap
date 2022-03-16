5494a01c56d70313659a042eef766297e5450031
EarlyAnt <54763755@qq.com>
2022-03-16 12:17:27 +0800

    1.新建PixiJS项目。 2.实现hello world。

---
+++ PixiJS/simple_http_server.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, jsonify, send_file, render_template
+
+app = Flask(__name__)
+
+@app.route('/<page>', methods = ['GET'])
+def home(page):
+    if request.method == 'GET':
+        return render_template(page, input_text = '', res_text = '')
+
+@app.route('/upload',methods = ['POST'])
+def upload_file():
+    for filename, file_obj in request.files.items():
+        if file_obj and filename:
+            file_obj.save('static/' + filename)
+            
+        #file = open('file_result.txt', 'w')
+        #file.write(filename)
+        #file.close()
+        
+    return jsonify(msg="OK")
+    
+@app.route('/download/<filename>')
+def download_file(filename):
+	return send_file('static/' + filename, as_attachment=True)
+
+app.run(
+      #host='192.168.0.103',
+      host='0.0.0.0',
+      port= 5000,
+      debug=True
+    )
